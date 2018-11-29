9378bc18cbdcf35a462c9252fcdbdc027299a897
Kevin Alejandro <37127939+TheCodeBite@users.noreply.github.com>
2018-11-29 11:25:28 -0600

    Actualizacion 1.0

--- DragonFeliz/src/dragonfeliz/FXMLDocumentController.java
+++ DragonFeliz/src/dragonfeliz/FXMLDocumentController.java
@@ -5,22 +5,26 @@
  */
 package dragonfeliz;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -28,49 +32,72 @@ import javafx.stage.Stage;
  * @author Kevin Alejandro
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private Button btn_Login;
+    
+    @FXML
+    private Button btn_Cerrar;
 
     @FXML
     private TextField tf_Password;
 
     @FXML
     private TextField tf_USer;
+    
+    @FXML
+    private Pane P_Error;
+    
 
+    String user;
+    String password;
+
+    Alert alert = new Alert(Alert.AlertType.WARNING);
 
     @FXML
     void OnAction_Login(ActionEvent event) throws IOException {
-    /*    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FXMLVentas.fxml"));
-        Parent root1 = (Parent) fxmlLoader.load();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(root1));
-        stage.setResizable(false);
-        stage.setTitle("Restaurant - El Dragon Feliz");
-        Image ico = new Image("/img/dragon.png"); 
-        stage.getIcons().add(ico);
 
-        stage.show(); */
+        user = tf_USer.getText();
+        password = tf_Password.getText();
         
-        Stage stage = new Stage();
+        if (user.length() == 0 || password.length() == 0 || user.length() == 0 && password.length() == 0) {
+            //warnningAlert("Error", "Introduzca Usuario y/o Contraseña", "Se necesitan parametros");
+            P_Error.setVisible(true);
+        } else {
+            Stage stage = new Stage();
 
-                        stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
-                        stage.close();
-                        Parent root = null;
-                        try {
-                            root = FXMLLoader.load(getClass().getResource("FXMLVentas.fxml"));
-                        } catch (IOException ex) {
-                                Logger.getLogger(FXMLVentasController.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        Scene scene = new Scene(root);
-                        stage.setScene(scene);
-                        stage.setTitle("Restaurant - El Dragon Feliz - Ventas");
-                        stage.show();
+            stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
+            stage.close();
+            Parent root = null;
+            try {
+                root = FXMLLoader.load(getClass().getResource("FXMLVentas.fxml"));
+            } catch (IOException ex) {
+                Logger.getLogger(FXMLVentasController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Restaurant - El Dragon Feliz - Ventas");
+            stage.show();
+
+        }
     }
     
+    @FXML
+    void OnAction_Cerrar(ActionEvent event) throws IOException {
+        P_Error.setVisible(false);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        P_Error.setVisible(false);
         // TODO
-    }    
-    
+    }
+
+    public void warnningAlert(String title, String header, String content) {
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
 }
--- DragonFeliz/src/dragonfeliz/FXMLVentasController.java
+++ DragonFeliz/src/dragonfeliz/FXMLVentasController.java
@@ -1,77 +1,48 @@
 package dragonfeliz;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
-
+import javafx.stage.Stage;
 
 public class FXMLVentasController {
 
     @FXML
-    private ResourceBundle resources;
+    private Button btn_Agregar, btn_Cobrar, btn_Borrar;
 
     @FXML
-    private URL location;
+    private Button btn_Aguacate, btn_Huevo;
 
     @FXML
-    private Button btn_Agregar;
+    private Button btn_Algas, btn_Apio, btn_Brocoli, btn_Cebolla, btn_Chicharo, btn_Papa, btn_Zanahoria;
 
     @FXML
-    private Button btn_Aguacate;
+    private Button btn_Arroz, btn_Pasta;
 
     @FXML
-    private Button btn_Algas;
+    private Button btn_Base, btn_Carne, btn_Salsas, btn_Toping, btn_Verdura;
 
     @FXML
-    private Button btn_Apio;
+    private Button btn_Pescado, btn_Pollo, btn_Puerco, btn_Res, btn_Oveja;
 
     @FXML
-    private Button btn_Arroz;
+    private Button btn_s1, btn_s2, btn_s3;
 
     @FXML
-    private Button btn_Base;
-
-    @FXML
-    private Button btn_Borrar;
-
-    @FXML
-    private Button btn_Brocoli;
-
-    @FXML
-    private Button btn_Carne;
-
-    @FXML
-    private Button btn_Cebolla;
-
-    @FXML
-    private Button btn_Chicharo;
-
-    @FXML
-    private Button btn_Cobrar;
-
-    @FXML
-    private Button btn_Huevo;
-
-    @FXML
-    private Button btn_Oveja;
-
-    @FXML
-    private Button btn_Papa;
-
-    @FXML
-    private Button btn_Pasta,btn_Pescado,btn_Pollo,btn_Puerco,btn_Res;
-
-    @FXML
-    private Button btn_Salsas, btn_Toping, btn_Verdura,btn_Zanahoria;
-
-    @FXML
-    private Button btn_s1,btn_s2,btn_s3;
+    private Button btn_Salir;
 
     @FXML
     private TableColumn<?, ?> tc_ID;
@@ -88,78 +59,71 @@ public class FXMLVentasController {
     @FXML
     private Text tx_Total;
 
-
     @FXML
     void initialize() {
-       btn_Base.setOnAction(new EventHandler<ActionEvent>() {
-           @Override
-           public void handle(ActionEvent event) {
-               ocultar_todo();//To change body of generated methods, choose Tools | Templates.
-               btn_Arroz.setVisible(true);
-               btn_Pasta.setVisible(true);
-           }
-       });
-       btn_Carne.setOnAction(new EventHandler<ActionEvent>() {
-           @Override
-           public void handle(ActionEvent event) {
-               ocultar_todo();//To change body of generated methods, choose Tools | Templates.
-               btn_Puerco.setVisible(true);
-               btn_Res.setVisible(true);
-               btn_Pollo.setVisible(true);
-<<<<<<< HEAD
-               btn_Oveja.setVisible(true);
-               btn_Pescado.setVisible(true);
-               
-=======
-               btn_Pescado.setVisible(true);
-               btn_Oveja.setVisible(true);
-           }
-       });
-       btn_Verdura.setOnAction(new EventHandler<ActionEvent>() {
-           @Override
-           public void handle(ActionEvent event) {
-               ocultar_todo();//To change body of generated methods, choose Tools | Templates.
-               btn_Algas.setVisible(true);
-               btn_Apio.setVisible(true);
-               btn_Brocoli.setVisible(true);
-               btn_Cebolla.setVisible(true);
-               btn_Chicharo.setVisible(true);
-               btn_Zanahoria.setVisible(true);
-               btn_Papa.setVisible(true);
-           }
-       });
-       btn_Salsas.setOnAction(new EventHandler<ActionEvent>() {
-           @Override
-           public void handle(ActionEvent event) {
-               ocultar_todo();//To change body of generated methods, choose Tools | Templates.
-               btn_s1.setVisible(true);
-               btn_s2.setVisible(true);
-               btn_s3.setVisible(true);
-           }
-       });
-       btn_Toping.setOnAction(new EventHandler<ActionEvent>() {
-           @Override
-           public void handle(ActionEvent event) {
-               ocultar_todo();//To change body of generated methods, choose Tools | Templates.
-               btn_Huevo.setVisible(true);
-               btn_Aguacate.setVisible(true);
->>>>>>> 9c7209cc3a84b8086c36571669f6acbb44171244
-           }
-       });
-       btn_Arroz.setOnAction(new EventHandler<ActionEvent>() {
-           @Override
-           public void handle(ActionEvent event) {
-               System.out.println("SE AGREGO ARROZ");
-               ocultar_todo();//To change body of generated methods, choose Tools | Templates.
-           }
-       });
+        btn_Base.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ocultar_todo();//To change body of generated methods, choose Tools | Templates.
+                btn_Arroz.setVisible(true);
+                btn_Pasta.setVisible(true);
+            }
+        });
+        btn_Carne.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ocultar_todo();//To change body of generated methods, choose Tools | Templates.
+                btn_Puerco.setVisible(true);
+                btn_Res.setVisible(true);
+                btn_Pollo.setVisible(true);
+                btn_Oveja.setVisible(true);
+                btn_Pescado.setVisible(true);
+                btn_Pescado.setVisible(true);
+                btn_Oveja.setVisible(true);
+            }
+        });
+        btn_Verdura.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ocultar_todo();//To change body of generated methods, choose Tools | Templates.
+                btn_Algas.setVisible(true);
+                btn_Apio.setVisible(true);
+                btn_Brocoli.setVisible(true);
+                btn_Cebolla.setVisible(true);
+                btn_Chicharo.setVisible(true);
+                btn_Zanahoria.setVisible(true);
+                btn_Papa.setVisible(true);
+            }
+        });
+        btn_Salsas.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ocultar_todo();//To change body of generated methods, choose Tools | Templates.
+                btn_s1.setVisible(true);
+                btn_s2.setVisible(true);
+                btn_s3.setVisible(true);
+            }
+        });
+        btn_Toping.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ocultar_todo();//To change body of generated methods, choose Tools | Templates.
+                btn_Huevo.setVisible(true);
+                btn_Aguacate.setVisible(true);
+            }
+        });
+        btn_Arroz.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("SE AGREGO ARROZ");
+                ocultar_todo();//To change body of generated methods, choose Tools | Templates.
+            }
+        });
     }
-    
-    
-    void ocultar_todo(){
+
+    void ocultar_todo() {
         btn_Arroz.setVisible(false);
         btn_Pasta.setVisible(false);
-        
         btn_Aguacate.setVisible(false);
         btn_Algas.setVisible(false);
         btn_Apio.setVisible(false);
@@ -176,10 +140,25 @@ public class FXMLVentasController {
         btn_Zanahoria.setVisible(false);
         btn_s1.setVisible(false);
         btn_s2.setVisible(false);
-        btn_s3.setVisible(false);   
+        btn_s3.setVisible(false);
     }
-    
-    
-    
+
+    @FXML
+    void OnAction_Salir(ActionEvent event) {
+        Stage stage = new Stage();
+
+        stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
+        stage.close();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Restaurant - El Dragon Feliz - Login");
+        stage.show();
+    }
 
 }
